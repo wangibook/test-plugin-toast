57722d445bb8e1d6235d5d2ae7f538fc3a41e231
wangyong <wangyong@bdxdoctor.com>
2019-09-18 11:32:16 +0800

    test-pugin-toast插件

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+
+    <button @click="handleClick">点击触发子组件的toast</button>
+    <test-toast ref="toast"></test-toast>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      testArr: [1,2,6]
+    }
+  },
+  creted() {
+
+  },
+  methods: {
+    handleClick() {
+      this.$nextTick(() => {
+        this.$refs.toast.toastPlugin('在父组件里触发调用的toast',2000)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
